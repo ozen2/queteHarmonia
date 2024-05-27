6fa8c775daa564e81e7e05e57a519a3ce1b91644
Enzo Michel <enzomichel399@gmail.com>
2024-05-27 20:36:44 +0200

    commit

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,19 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+create table program (
+    id int unsigned primary key auto_increment not null,
+      title varchar(255) not null,
+      synopsis text not null,
+      poster varchar(255) not null,
+      country varchar(100) not null,
+      year int unsigned not null,
+        category_id int unsigned not null,
+  foreign key (category_id) references category(id)
+);
+
+create table category(
+  id int unsigned primary key auto_increment not null,
+  name varchar(250) not null
+);
